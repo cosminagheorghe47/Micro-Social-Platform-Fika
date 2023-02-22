b677d07d7106bf3d59d19237fd507ae4c63d77b5
cosminagheorghe <cosmi.2002@yahoo.com>
2023-02-23 00:17:50 +0200

    Add project files.

---
+++ MS_Fika/Data/Migrations/20221217170722_migratieeee.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MS_Fika.Data.Migrations
+{
+    public partial class migratieeee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Profiles_AspNetUsers_UserId",
+                table: "Profiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Profiles",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfileBio",
+                table: "Profiles",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles",
+                column: "UserId",
+                unique: true,
+                filter: "[UserId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Profiles_AspNetUsers_UserId",
+                table: "Profiles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Profiles_AspNetUsers_UserId",
+                table: "Profiles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Profiles",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ProfileBio",
+                table: "Profiles",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserId",
+                table: "Comments",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Profiles_UserId",
+                table: "Profiles",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_AspNetUsers_UserId",
+                table: "Comments",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Profiles_AspNetUsers_UserId",
+                table: "Profiles",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ MS_Fika/Data/Migrations/20230107112402_friend1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MS_Fika.Data.Migrations
+{
+    public partial class friend1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friends_AspNetUsers_User1Id",
+                table: "Friends");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friends_AspNetUsers_User2Id",
+                table: "Friends");
+
+            migrationBuilder.DropColumn(
+                name: "Friend1Id",
+                table: "Friends");
+
+            migrationBuilder.DropColumn(
+                name: "Friend2Id",
+                table: "Friends");
+
+            migrationBuilder.RenameColumn(
+                name: "User2Id",
+                table: "Friends",
+                newName: "User2_Id");
+
+            migrationBuilder.RenameColumn(
+                name: "User1Id",
+                table: "Friends",
+                newName: "User1_Id");
+
+            migrationBuilder.RenameColumn(
+                name: "FriendAccepted",
+                table: "Friends",
+                newName: "Accepted");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Friends_User2Id",
+                table: "Friends",
+                newName: "IX_Friends_User2_Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Friends_User1Id",
+                table: "Friends",
+                newName: "IX_Friends_User1_Id");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RequestTime",
+                table: "Friends",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friends_AspNetUsers_User1_Id",
+                table: "Friends",
+                column: "User1_Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friends_AspNetUsers_User2_Id",
+                table: "Friends",
+                column: "User2_Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friends_AspNetUsers_User1_Id",
+                table: "Friends");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Friends_AspNetUsers_User2_Id",
+                table: "Friends");
+
+            migrationBuilder.DropColumn(
+                name: "RequestTime",
+                table: "Friends");
+
+            migrationBuilder.RenameColumn(
+                name: "User2_Id",
+                table: "Friends",
+                newName: "User2Id");
+
+            migrationBuilder.RenameColumn(
+                name: "User1_Id",
+                table: "Friends",
+                newName: "User1Id");
+
+            migrationBuilder.RenameColumn(
+                name: "Accepted",
+                table: "Friends",
+                newName: "FriendAccepted");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Friends_User2_Id",
+                table: "Friends",
+                newName: "IX_Friends_User2Id");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Friends_User1_Id",
+                table: "Friends",
+                newName: "IX_Friends_User1Id");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Friend1Id",
+                table: "Friends",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Friend2Id",
+                table: "Friends",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friends_AspNetUsers_User1Id",
+                table: "Friends",
+                column: "User1Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Friends_AspNetUsers_User2Id",
+                table: "Friends",
+                column: "User2Id",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.NoAction);
+        }
+    }
+}
